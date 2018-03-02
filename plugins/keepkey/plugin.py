6057679ddf8a79345d5b62020a718e55653d419c
Lucas Betschart <lucasbetschart@gmail.com>
2018-03-02 15:25:01 +0100

    UI: Rename Bitcoin to Feathercoin

--- plugins/keepkey/plugin.py
+++ plugins/keepkey/plugin.py
@@ -138,7 +138,7 @@ class KeepKeyCompatiblePlugin(HW_PluginBase):
         return client
 
     def get_coin_name(self):
-        return "Testnet" if constants.net.TESTNET else "Bitcoin"
+        return "Testnet" if constants.net.TESTNET else "Feathercoin"
 
     def initialize_device(self, device_id, wizard, handler):
         # Initialization method
