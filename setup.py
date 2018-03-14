ac52c36a27fc95b1fca0951811c927ee5e2e347c
Lucas Betschart <lucasbetschart@gmail.com>
2018-03-14 14:07:47 +0100

    Add Feathercoin Icon
    For the portable and standalone build Windows uses the icon
    set by Qt. Therefore, let PyQt5 use a light version for Windows
    in order to increase the contrast with the dark Windows taskbar.
    Also, since Gnome 3 (default on Ubuntu) also has a dark theme
    we will use the light icon there, too. To be consistent also
    make it light on macOS.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
             usr_share = os.path.expanduser('~/.local/share')
     data_files += [
         (os.path.join(usr_share, 'applications/'), ['electrum-ftc.desktop']),
-        (os.path.join(usr_share, icons_dirname), ['icons/electrum.png'])
+        (os.path.join(usr_share, icons_dirname), ['icons/electrum_light.png'])
     ]
 
 class BuildPyCommand(build_py):
