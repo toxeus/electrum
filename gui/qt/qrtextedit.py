f8d92527910c28094381ab8ca530445ebfa80fe4
Filip Gospodinov <f@gospodinov.ch>
2018-03-16 11:10:44 +0100

    UI: disable QR code scanning
    zbar, which is used for QR code scanning, is an old
    and unmaintained project. It didn't work in our tests.
    Let's disable what doesn't work.

--- gui/qt/qrtextedit.py
+++ gui/qt/qrtextedit.py
@@ -38,8 +38,6 @@ class ScanQRTextEdit(ButtonsTextEdit, MessageBoxMixin):
         self.allow_multi = allow_multi
         self.setReadOnly(0)
         self.addButton(":icons/file.png", self.file_input, _("Read file"))
-        icon = ":icons/qrcode_white.png" if ColorScheme.dark_scheme else ":icons/qrcode.png"
-        self.addButton(icon, self.qr_input, _("Read QR code"))
         run_hook('scan_text_edit', self)
 
     def file_input(self):
