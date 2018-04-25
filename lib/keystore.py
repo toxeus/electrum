Anilkumar18/electrum-ftc

BIP44 key derivation path
As per SLIP44 Feathercoin's coin type is 8.
Since Electrum wallets are interoperable between cryptocurrencies,
this needs to be corrected to prevent key reuse.

lib/keystore.py
@@ -673,7 +673,7 @@ is_bip32_key = lambda x: is_xprv(x) or is_xpub(x)
 
 def bip44_derivation(account_id, segwit=False):
     bip  = 49 if segwit else 44
-    coin = 1 if bitcoin.NetworkConstants.TESTNET else 0
+    coin = 1 if bitcoin.NetworkConstants.TESTNET else 8
     return "m/%d'/%d'/%d'" % (bip, coin, int(account_id))
 
 def from_seed(seed, passphrase, is_p2sh):

toxeus/electrum

lib/keystore.py
@@ -709,7 +709,7 @@ is_bip32_key = lambda x: is_xprv(x) or is_xpub(x)
 
 
 def bip44_derivation(account_id, bip43_purpose=44):
-    coin = 1 if constants.net.TESTNET else 0
+    coin = 1 if constants.net.TESTNET else 8
     return "m/%d'/%d'/%d'" % (bip43_purpose, coin, int(account_id))
 
 def from_seed(seed, passphrase, is_p2sh):